f2bc47ab56a2063b687c728966e4b2cc7424e520
SajuukThanatoskhar <gareth_willia64@hotmail.com>
2016-08-26 23:32:28 +1000

    Final build for the night ~yawn~

--- src/test_tyrannosphere/arena/Arena.java
+++ src/test_tyrannosphere/arena/Arena.java
@@ -1,6 +1,8 @@
 package test_tyrannosphere.arena;
 
 import java.util.ArrayList;
+import java.util.List;
+import test_tyrannosphere.Fleet_Ship;
 
 /**
  * The arena is the backend where the fleets do battle
@@ -10,14 +12,20 @@ import java.util.ArrayList;
 public class Arena {
     
     private float radius;
-    private int time; 
-    private ArrayList numberoffleets;
+    private int time; //We start at 0, always.  Time is measured in seconds, because EVE is lovely like that
+    public List<Fleet_Point> Arena_Fleets = new ArrayList<>();  //Keeping a list of all the unique fleet_points for this Arena
     
     
-    public Arena(ArrayList fleetnumber, float size)
+    public Arena(float size)
     {
-        this.numberoffleets = fleetnumber;
+        //this.numberoffleets = fleetnumber;
         this.radius = size;
+        this.time = 0; //Starting at 0
+    }
+    
+    public void addfleet(Fleet_Point new_fleet)
+    {
+        //TODO: Functionality to add new fleet_point(!) to arena
     }
     
     public float getRadius() {
@@ -36,9 +44,16 @@ public class Arena {
         this.time = time;
     }
     
+    
+    /*
+    This function is the time+1 fleet updater
+    */
     public void fleets_update()
     {
         
+        
+        
+        this.setTime(this.getTime() + 1);
     }
     
 }
--- src/test_tyrannosphere/arena/Fleet_Point.java
+++ src/test_tyrannosphere/arena/Fleet_Point.java
@@ -5,12 +5,17 @@ import test_tyrannosphere.Fleet;
 
 /**
  * In test_tyr.Fleet, we have made our fleet and the ships that go in that fleet
+ * The fleets are then represented as points with vectors to represent them fighting as a unit.  
+ * Maybe later we can individual ships being represented on the field as opposed to entire fleets
  * @author Sajuuk
  */
 public class Fleet_Point {
     
-    private Fleet This_Fleet;
-    private float x_pos,y_pos,z_pos;
+    private Fleet This_Fleet;  //The fleet attached to this Fleet_Point
+    private float x_pos,y_pos,z_pos;  //All fleets have a starting point at time 0.  
+    private float vector[];  //All fleets have a vector (for their speed)
+    
+    
     
     /*
     Initialise the fleet_point, assigning a fleet and an initial x,y,z location
@@ -19,8 +24,26 @@ public class Fleet_Point {
     {
         this.This_Fleet = myfleet;
         this.x_pos = x;
-        this.y_pos = y;
-        this.z_pos = z;
+        this.y_pos = y; 
+        this.z_pos = z; 
         
     }
+    
+    //As it says on the tin, we are approaching another fleet
+    public void approach_fleet(float range, Fleet_Point target)
+    {
+        //TODO
+    }
+    
+    public void orbit_fleet(float range, Fleet_Point target)
+    {
+        //TODO
+    }
+    
+    public void keep_at_range_fleet(float range, Fleet_Point target)
+    {
+        //TODO
+    }
+    
+    
 }
